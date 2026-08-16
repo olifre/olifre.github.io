c63f996f0fafa4ef2998d603e09f4e91dbb59bd9
Oliver Freyermuth <o.freyermuth@googlemail.com>
2026-08-16 20:38:21 +0200

    add favicon and generation script
    Signed-off-by: Oliver Freyermuth <o.freyermuth@googlemail.com>

---
+++ static/gen_favicons.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+INPUT="favicon.svg"
+
+if [ ! -f "$INPUT" ]; then
+  echo "Error: $INPUT not found!"
+  exit 1
+fi
+
+echo "Generating favicons from $INPUT..."
+
+# Array of target filenames and dimensions
+declare -A ICONS=(
+  ["apple-touch-icon.png"]="180"
+  ["favicon-32x32.png"]="32"
+  ["favicon-16x16.png"]="16"
+  ["mstile-150x150.png"]="150"
+  ["android-chrome-192x192.png"]="192"
+  ["android-chrome-512x512.png"]="512"
+)
+
+# Loop through each target and convert
+for filename in "${!ICONS[@]}"; do
+  size="${ICONS[$filename]}"
+  echo "Generating $filename (${size}x${size})..."
+  inkscape "$INPUT" --export-filename="$filename" -w "$size" -h "$size"
+  optipng -o4 "$filename" 2> /dev/null
+  advpng -z4 "$filename" > /dev/null
+done
+
+echo "Done! All icons created successfully."
